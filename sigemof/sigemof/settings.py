97fe15e5fb47d7a0cb5e5e54ece132092191388b
kimba97 <32586553+kimba97@users.noreply.github.com>
2019-07-05 19:48:56 -0600

    Update settings.py

--- sigemof/sigemof/settings.py
+++ sigemof/sigemof/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qxn7_b9b=vw!5*=(e#_i=18k-=8ek(5$uw4x)t=)lm!dxpbp1x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -121,10 +121,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#    )
+STATIC_ROOT = './sigemof/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
 # https://docs.djangoproject.com/en/1.11/howto/static-files
 
 
