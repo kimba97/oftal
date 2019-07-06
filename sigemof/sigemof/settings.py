a100fa15acfa3b8b5779d947d42f4c70eedfd092
kimba97 <32586553+kimba97@users.noreply.github.com>
2019-07-05 20:54:26 -0600

    Update settings.py

--- sigemof/sigemof/settings.py
+++ sigemof/sigemof/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ('/home/oftal/oftal.pythonanywhere.com/sigemof/static/')
+STATICFILES_DIRS = (('assets', '/home/oftal/oftal.pythonanywhere.com/sigemof/static/'),)
 # https://docs.djangoproject.com/en/1.11/howto/static-files
 
 
