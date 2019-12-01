31a8b8b63054a12353bfca1d1bf91b383bbf0a75
lordspider47 <43802959+lordspider47@users.noreply.github.com>
2019-12-01 15:06:43 -0600

    p

--- sigemof/oftal/views.py
+++ sigemof/oftal/views.py
@@ -557,14 +557,14 @@ class ReportePacientesPDF(View):
     def tabla(self,pdf,y):
         encabezados = ('Nombres', 'Apellidos', 'DUI', 'Edad', 'Sexo', 'Telefono', 'Correo')
         detalles = [(paciente.nombrePersona, paciente.apellidoPersona, paciente.dui, paciente.edad, paciente.sexo, paciente.telefono, paciente.correo) for paciente in Paciente.objects.all()]
-        detalleOrden = Table([encabezados] + detalles, colWidths=[0.7*inch, 0.8*inch, 0.8*inch, 0.8*inch, 0.8*inch, 0.8*inch, 1.5*inch])
+        detalleOrden = Table([encabezados] + detalles, colWidths=[1.1*inch, 1.1*inch, 0.9*inch, 0.8*inch, 0.8*inch, 0.8*inch, 2*inch])
         detalleOrden.setStyle(TableStyle([
             ('ALIGN',(0,0),(3,0),'CENTER'),
             ('GRID', (0, 0), (-1, -1), 1, colors.black),
             ('FONTSIZE', (0, 0), (-1, -1), 10),
             ]))
         detalleOrden.wrapOn(pdf, 800, 500)
-        detalleOrden.drawOn(pdf, 100, y)
+        detalleOrden.drawOn(pdf, 50, y)
 
 
 class ReporteExpedientesPDF(View):
